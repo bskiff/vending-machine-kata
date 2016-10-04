3bab6eb2950ba5ca1326c4214127338747cbea4a
Ben Skiff <benskiff@msn.com>
2016-10-04 00:16:58 -0500

    refactor test into describe block

--- test/vending-machine.spec.js
+++ test/vending-machine.spec.js
@@ -3,87 +3,90 @@ const should = require('should');
 
 describe('Vending Machine', () => {
     var vendingMachine;
+    
     beforeEach(() => {
         vendingMachine = VendingMachine();
     });
 
-    it('should display INSERT COIN when no coins have been inserted', () => {
-        const value = vendingMachine.getDisplayed();
+    describe(`with 'Make Change' feature`, () => {
+        it('should display INSERT COIN when no coins have been inserted', () => {
+            const value = vendingMachine.getDisplayed();
 
-        should.equal(value, 'INSERT COIN');
-    });
-
-    it('should accept nickels', () => {
-        vendingMachine.insertCoin({
-           type: "nickel"
+            should.equal(value, 'INSERT COIN');
         });
-        const value = vendingMachine.getDisplayed();
 
-        should.equal(value, 5);
-    });
+        it('should accept nickels', () => {
+            vendingMachine.insertCoin({
+                type: "nickel"
+            });
+            const value = vendingMachine.getDisplayed();
 
-    it('should accept dimes', () => {
-        vendingMachine.insertCoin({
-            type: "dime"
+            should.equal(value, 5);
         });
-        const value = vendingMachine.getDisplayed();
 
-        should.equal(value, 10);
-    });
+        it('should accept dimes', () => {
+            vendingMachine.insertCoin({
+                type: "dime"
+            });
+            const value = vendingMachine.getDisplayed();
 
-    it('should accept quarters', () => {
-        vendingMachine.insertCoin({
-            type: "quarter"
+            should.equal(value, 10);
         });
-        const value = vendingMachine.getDisplayed();
 
-        should.equal(value, 25);
-    });
+        it('should accept quarters', () => {
+            vendingMachine.insertCoin({
+                type: "quarter"
+            });
+            const value = vendingMachine.getDisplayed();
 
-    it('should display aggregate value of added coins', () => {
-        vendingMachine.insertCoin({
-            type: "nickel"
+            should.equal(value, 25);
         });
-        should.equal(vendingMachine.getDisplayed(), 5);
 
-        vendingMachine.insertCoin({
-            type: "dime"
+        it('should display aggregate value of added coins', () => {
+            vendingMachine.insertCoin({
+                type: "nickel"
+            });
+            should.equal(vendingMachine.getDisplayed(), 5);
+
+            vendingMachine.insertCoin({
+                type: "dime"
+            });
+            should.equal(vendingMachine.getDisplayed(), 15);
+
+            vendingMachine.insertCoin({
+                type: "quarter"
+            });
+            should.equal(vendingMachine.getDisplayed(), 40);
         });
-        should.equal(vendingMachine.getDisplayed(), 15);
 
-        vendingMachine.insertCoin({
-            type: "quarter"
+        it('should place rejected coins in coin return', () => {
+            const insertedCoin = {
+                type: "chocolate"
+            };
+            vendingMachine.insertCoin(insertedCoin);
+
+            should.equal(vendingMachine.getDisplayed(), 'INSERT COIN');
+            const returnedCoins = vendingMachine.getReturnedCoins();
+
+            should.deepEqual(returnedCoins, [insertedCoin]);
         });
-        should.equal(vendingMachine.getDisplayed(), 40);
-    });
 
-    it('should place rejected coins in coin return', () => {
-        const insertedCoin = {
-            type: "chocolate"
-        };
-        vendingMachine.insertCoin(insertedCoin);
+        it('should have no returned coins at start', () => {
+            const returnedCoins = vendingMachine.getReturnedCoins();
 
-        should.equal(vendingMachine.getDisplayed(), 'INSERT COIN');
-        const returnedCoins = vendingMachine.getReturnedCoins();
+            returnedCoins.should.be.empty();
+        });
 
-        should.deepEqual(returnedCoins, [insertedCoin]);
-    });
+        it('should return all rejected coins', () => {
+            const expectedCoins = [
+                {type: "chocolate"},
+                {type: "Canadian coin, eh"}
+            ];
+            vendingMachine.insertCoin(expectedCoins[0]);
+            vendingMachine.insertCoin(expectedCoins[1]);
+            const returnedCoins = vendingMachine.getReturnedCoins();
 
-    it('should have no returned coins at start', () => {
-        const returnedCoins = vendingMachine.getReturnedCoins();
-
-        returnedCoins.should.be.empty();
-    });
-
-    it('should return all rejected coins', () => {
-        const expectedCoins = [
-            { type: "chocolate"},
-            { type: "Canadian coin, eh"}
-        ];
-        vendingMachine.insertCoin(expectedCoins[0]);
-        vendingMachine.insertCoin(expectedCoins[1]);
-        const returnedCoins = vendingMachine.getReturnedCoins();
-
-        should.deepEqual(returnedCoins, expectedCoins);
+            should.deepEqual(returnedCoins, expectedCoins);
+        });
     });
 });
